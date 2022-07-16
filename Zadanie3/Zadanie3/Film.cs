2b5ef5777bf803d09b636604ce87506ce234a968
Jakub Wachowicz <82938429+kubicaprim@users.noreply.github.com>
2022-07-16 16:18:47 +0200

    Add files via upload

---
+++ Zadanie3/Zadanie3/Film.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_INIS4_PR2._2_z4
+{
+    public class Film : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly static Dictionary<string, string[]> relatedProperties = new Dictionary<string, string[]>()
+        {
+            ["Tytuł"] = new string[] { "TytułReżyser", "FormatListy" },
+            ["Reżyser"] = new string[] { "TytułReżyser", "FormatListy" },
+            ["DataNagrania"] = new string[] { "Wiek", "FormatListy" },
+            ["Wydawca"] = new string[] { "Wydawca", "FormatListy" },
+            ["Nośnik"] = new string[] { "Nośnik", "FormatListy" }
+        };
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (relatedProperties.ContainsKey(propertyName))
+                foreach (string relatedProperty in relatedProperties[propertyName])
+                    OnPropertyChanged(relatedProperty);
+            //śledzenie zapobiegające stack overflow?
+        }
+
+        static uint następneID = 0;
+        /*uint wiek = 0;*/
+        string
+            tytuł,
+            reżyser,
+            wydawca,
+            nośnik
+            ;
+        DateTime?
+            dataNagrania = null
+            ;
+
+        public string Tytuł
+        {
+            get => tytuł;
+            set
+            {
+                tytuł = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Reżyser
+        {
+            get => reżyser;
+            set
+            {
+                reżyser = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Wydawca
+        {
+            get => wydawca;
+            set
+            {
+                wydawca = value;
+
+            }
+        }
+        public string Nośnik
+        {
+            get => nośnik;
+            set
+            {
+                nośnik = value;
+
+            }
+        }
+        public string WiekFilmu
+        {
+            get
+            {
+                if (dataNagrania != null)
+                {
+                    DateTime koniec;
+                    koniec = DateTime.Now;
+                    return ((koniec - (DateTime)dataNagrania).Days / 365).ToString();
+                }
+                else
+                    return "Brak informacji";
+            }
+        }
+        public DateTime? DataNagrania
+        {
+            get => dataNagrania;
+            set
+            {
+                dataNagrania = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TytułReżyser { get => $"{tytuł}, {reżyser}"; }
+        public string FormatListy { get => $"{tytuł}, {reżyser}, {WiekFilmu} lat"; }
+        public uint ID { get; } = następneID++;
+        public string FormatID { get => "ID: " + ID; }
+    }
+}
---
+++ Zadanie3/Zadanie3/Model.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_INIS4_PR2._2_z4
+{
+    class Model
+    {
+        /*Prawdopodobnie brakuje jawnego pola z listą, właściwości jawnie zaimplementowanej i notyfikacji o zmianie*/
+        public LinkedList<Film> Lista { get; set; } = new LinkedList<Film>(new Film[] {
+            new Film() {
+                Tytuł = "Skazani na Shawshank",
+                Reżyser = "Frank Darabont",
+                Wydawca = "Castle Rock Entertainment",
+                Nośnik = "DVD",
+                DataNagrania = DateTime.Parse("16.04.1995")
+            },
+            new Film() {
+                Tytuł = "Zielona mila",
+                Reżyser = "Frank Darabont",
+                Wydawca = "Warner Bros. Pictures",
+                Nośnik = "Bluray",
+                DataNagrania = DateTime.Parse("09.12.1999")
+            },
+            new Film() {
+                Tytuł = "Forrest Gump",
+                Reżyser = "Robert Zemeckis",
+                Wydawca = "Paramount Pictures",
+                Nośnik = "BluRay i DVD",
+                DataNagrania = DateTime.Parse("06.07.1994")
+            },
+        });
+
+        internal void OtwórzSzczegóły(Film wybrany)
+        {
+            Szczegóły szczegóły = new Szczegóły(wybrany);
+            szczegóły.Show();
+        }
+        internal void DodajNowy()
+        {
+            Film nowa = new Film();
+            Lista.AddLast(nowa);
+            Szczegóły szczegóły = new Szczegóły(nowa);
+            szczegóły.Show();
+            /*aktualizowanie widoku samej listy*/
+        }
+    }
+}
